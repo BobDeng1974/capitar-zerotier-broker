9ccd4cd6dc14b9945c787c0b73ec1bad25c59abd
Janjaap Bos <janjaap.bos@capitar.com>
2018-10-10 11:55:58 +0200

    frontend

---
+++ static/proxy/app/zerotier/admin/main.vue
@@ -0,0 +1,28 @@
+<template>
+<div><div>
+
+  <navbar></navbar>
+  <login></login>
+
+</div></div>
+</template>
+
+<script>
+
+module.exports = {
+  data () {
+    return {
+    }
+  },
+  props: [],
+  computed: {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
